24dafe630e4d0cd27b5f924723bbe730c838b8e4
Subodh <subodhss23@gmail.com>
2019-10-28 16:54:06 -0700

    boiler palte

---
+++ api/routes/testAPI.js
@@ -0,0 +1,9 @@
+let express = require('express');
+let router = express.Router();
+
+
+router.get('/', function(req, res, next){
+    res.send('API is working properly');
+});
+
+module.exports = router;
